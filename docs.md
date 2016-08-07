6fd658cbde16d8ddb30d15f48a4d4d0280fe8c19
liam4 <towerofnix@gmail.com>
2016-08-06 23:50:39 -0300

    Less initial commit

---
+++ docs.md
@@ -0,0 +1,35 @@
+# [Documentation](id:documentation)
+
+## [Program Compiling](id:program-compiling)
+
+For each line of a program, the following occurs:
+
+* If the line is an empty string, continue to the next line.
+* If the line starts with `define`, behave as described in [Function Definitions](#function-definitions).
+* If the line starts with `^`, behave as described in [Procedure Calls](#procedure-calls).
+* Otherwise, behave as described in [Basic Command Blocks](#basic-command-blocks).
+
+## [Basic Command Blocks](id:basic-command-blocks)
+
+Each line of a program that does *not* start with a `^` is a command block line.
+
+All the text on the line represents a single command block. *(TODO: This isn't going to be true; e.g. command block attributes, function calls, and so on.)*
+
+## [Function Definitions](id:function-definitions)
+
+Functions are defined by lines of a program that start with `define`. The syntax is as follows:
+
+    define [name]()
+      [line 1]
+      [line 2]
+      [...]
+
+When a function definition is found, the code and name will be added to a table of function definitions specific to the progam.
+
+## [Procedure Calls](id:procedure-calls)
+
+Procedure calls are used to insert code into multiple parts of your program. The syntax is as follows:
+
+    [START OF LINE]^[name]()
+
+When a procedure call is found, the code that is respective to the name will be found from a program-specific function table, and inserted into the result in place of the procedure call.
--- main.js
+++ main.js
@@ -1,1 +1,121 @@
-// https://www.youtube.com/watch?v=e-ORhEE9VVg
+class Program {
+  constructor(code) {
+    this.code = code
+    this.functions = {}
+  }
+
+  compile() {
+    const code = this.code
+    const lines = code.split('\n')
+    const results = []
+
+    LINE_LOOP:
+    for (let lineIndex = 0; lineIndex < lines.length; lineIndex++) {
+      let line = lines[lineIndex]
+
+      if (line === '')
+        continue
+
+      if (line.startsWith('define')) {
+        // Getting name
+        let name = ''
+
+        let nameCharIndex = 6
+        while (true) {
+          nameCharIndex++
+          const char = line[nameCharIndex]
+
+          if (nameCharIndex >= line.length)
+            break
+
+          if (char === '(')
+            break
+
+          name += char
+        }
+
+        // Getting code lines
+        const codeLines = []
+
+        while (true) {
+          lineIndex++
+          line = lines[lineIndex]
+
+          if (lineIndex > lines.length)
+            break
+
+          if (!line.startsWith('  '))
+            break
+
+          codeLines.push(line.slice(2))
+        }
+
+        // Storing function
+        this.functions[name] = codeLines
+
+        continue
+      }
+
+      // Parsing command, doing expressions etc.
+      let command = ''
+      let charIndex = -1
+
+      CHAR_LOOP:
+      while (true) {
+        charIndex++
+        let char = line[charIndex]
+
+        if (charIndex >= line.length) break
+
+        if (char === '^') {
+          let name = ''
+          const oldCharIndex = charIndex
+
+          while (true) {
+            charIndex++
+            char = line[charIndex]
+            if (charIndex >= line.length) break
+
+            if (char === '(') break
+
+            name += char
+          }
+
+          const codeLines = this.functions[name]
+
+          if (oldCharIndex === 0) {
+            for (let line of codeLines)
+              results.push({type: 'c', command: line})
+
+            continue LINE_LOOP
+          } else {
+            console.log('Function expressions not implemented yet')
+
+            charIndex++
+
+            continue CHAR_LOOP
+          }
+        }
+
+        command += char
+      }
+
+      const obj = {type: 'c', command}
+
+      results.push(obj)
+    }
+
+    return results
+  }
+}
+
+console.dir(new Program(`
+
+define foo()
+  say foo
+  say bar
+
+say hi
+^foo()
+
+`).compile())
